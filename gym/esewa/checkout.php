745fcc593c76a5bcd159c0b2e1a33ab836d93879
Suraj Singh <surajsubin5@gmail.com>
2024-07-21 23:21:31 +0545

    message

--- gym/esewa/checkout.php
+++ gym/esewa/checkout.php
@@ -30,17 +30,22 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 	</head>
+	<style>
+		.btn:focus {
+			box-shadow:none !important;
+		 }
+	</style>
 	<body>
 		<div class="container">
 			<div class="pt-md-5">
-				<div class="col-md-12">
-					<h2>Order Details</h2>
-					<div class="row">
+				<div class="mx-auto" style="width: 350px; height:80px;">
+					<h2 class="text-center">Order Details</h2>
+					<div class="col">
 						
 						
-						<div class="col-md-4">
-							<div class="card" stylea="width: 18rem;">
-								<div class="imagecontainer" style="height: 400px;">
+						<div class="col-md-4" >
+							<div class="card" style="width: 18rem;height: 470px;;">
+								<div class="imagecontainer" style="height: 250px;">
 									<img src="image/<?php echo $product['image']?>" class="card-img-top" alt="..." style="width: 100%; height: 100%;">
 								</div>
 								<div class="card-body">
@@ -50,31 +55,32 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
 								</div>
 							</div>
 						</div>
-						<div class="col-md-6">
-							<h3>Pay With</h3>
-							<ul class="list-group">
-								<li class="list-group-item">
+						
+						<div class="mx-auto" style="width: 200px;" >
+							<h3 class="text-center  pt-3"  >Pay With</h3>
+							<ul class="list-group ">
+								<li class="list-group-item " style="border:none;">
 									<form action="https://uat.esewa.com.np/epay/main" method="POST">
 										<input value="<?php echo $total;?>" name="tAmt" type="hidden">
 										<input value="<?php echo $total;?>" name="amt" type="hidden">
 										<input value="0" name="txAmt" type="hidden">
 										<input value="0" name="psc" type="hidden">
-										<input value="0" name="pdc" type="hidden">
+
+										<input type="hidden" name="pdc" value="0">
 										<input value="epay_payment" name="scd" type="hidden">
-										<input value="<?php echo $invoice_no;?>" name="pid" type="hidden">
+										<input type="hidden" name="pid" value="<?php echo $invoice_no;?>"  >
 										<input value="http://tandubhai.com/esewa/esewa_payment_success.php" type="hidden" name="su">
 										<input value="http://tandubhai.com/esewa/esewa_payment_failed.php" type="hidden" name="fu">
-										<input type="image" src="image/esewa.png">
-										</li>
-										<li class="list-group-item"><input type="image" src="image/fonepay.png"></li>
-										<li class="list-group-item"><input type="image" src="image/khalti.png"></li>
-										<li class="list-group-item"><input type="image" src="image/connectips.png"></li>
-										<li class="list-group-item"><input type="image" src="image/hbl.png"></li>
+										<div class="text-center " >
+										<button  type="submit" class="btn btn-success" style="color: white; border: none; padding: 10px 20px; cursor: pointer;">eSewa</button>
+										</div>
+									</li>
 									</ul>
 								</div>
 								
 							</div>
 						</div>
+						
 					</div>
 				</div>
 			</body>
